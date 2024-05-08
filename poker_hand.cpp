a268ea3cc71a3482f49aa0e2fffde96e66c343e5
Amir Mehrabian <Amir.mehrabian1993@gmail.com>
2024-05-08 22:44:09 +0330

    updated poker hand

--- poker_hand.cpp
+++ poker_hand.cpp
@@ -15,20 +15,20 @@ void print_arr(int size, T arr[]){
 
 
 bool is_straight(string arr[]){
-    
+
     unordered_set<string> myvals = {arr[0],arr[1],arr[2],arr[3],arr[4]};
-    
+
     for(int i=0; i<9;i++){
         unordered_set<string> vals={values[i], values[i+1], values[i+2], values[i+3], values[i+4]};
-        
+
          if (myvals ==vals){
             return true;
             }
-        
+
         }
-        
+
     return false;
-    
+
     }
 
 
@@ -36,71 +36,107 @@ bool is_straight(string arr[]){
 
 /* num_repeat function can give us the sum of number of duplication. Based on
 its output, we realize that if output is 1: pair, 2: two pair, 3:three of a kind, 6: four of a kind, 4:full house.*/
-    
+
 int num_repeat(string arr[]){
     int rep = 0;
-    
+
     for (int i=0; i<4; i++){
         for (int j =i+1; j<5; j++){
-            
+
             if (arr[i]==arr[j]){
                 rep +=1;
-                
+
                 }
-            
+
             }
     }
-    
-    return rep; 
+
+    return rep;
 }
+
+bool is_flush(string arr[]){
+    if(num_repeat(arr)==10){
+        return true;
+    }else{
+        return false;
+    }
+
+}
+
+
+
+string hand_type(string vals[], string suits[]){
+string type;
+if (is_straight(vals)){
+
+    type = "straight";
+
+
+}else if(is_flush(suits)){
+    type = "flush";
+
+}else if(num_repeat(vals)!=0){
+
+    type = "pair";
+
+}else{
+   type = "Higher_Hand";
+}
+return type;
+
+}
+
+
 
 int main(){
     //print_arr<char>(4,suits);
     //print_arr<string>(13,values);
     string str, word;
-    
+
     string my_val[5], my_suit[5];
     getline(cin,str);
-    
+
     int len = str.length(), cntr = 0, l_w;
-    
+
     for (int i =0; i<len; i++){
         if(str[i] == ' '){
-         
-        l_w = word.length(); 
+
+        l_w = word.length();
         my_val[cntr] = word.substr(0,l_w-1) ;
         my_suit[cntr] =word.substr(l_w-1,1) ;
         cntr += 1;
         word = "";
-        
+
         }else if(i == len-1){
-            
+
         word +=str[i];
-        l_w = word.length(); 
+        l_w = word.length();
         my_val[cntr] = word.substr(0,l_w-1) ;
         my_suit[cntr] =word.substr(l_w-1,1) ;
         cntr += 1;
         word = "";
-            
+
         }else{
-            
+
         word +=str[i];
-        
+
         }
-        
-        
-    
+
+
+
     }
     cout<<"---------------"<<endl;
     print_arr<string>(5,my_suit);
     cout<<"values"<<endl;
-    
+
     print_arr<string>(5,my_val);
-    
+
   cout<<num_repeat(my_val)<<endl;
   cout<<"Is it straight:"<<is_straight(my_val)<<endl;
-    
-    
+  cout<<"Is it flush:"<< is_flush(my_suit)<<endl;
+  cout<<"Hand type is: "<< hand_type(my_val, my_suit)<<endl;
+
+
     return 0;
-    
+
 }
